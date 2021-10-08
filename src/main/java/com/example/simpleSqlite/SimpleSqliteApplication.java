9869ac3adb4162ccb33ef5e984b8431953131e36
happy <hanbin615@hotmail.com>
2021-10-08 14:30:43 +0800

    set up maven and springboot

---
+++ src/main/java/com/example/simpleSqlite/SimpleSqliteApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleSqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSqliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSqliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleSqlite/SimpleSqliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleSqlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSqliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
